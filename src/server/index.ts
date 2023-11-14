0428b63c0ec7af4bd27d66cf8889bdf7350dce46
Mauro Colucci <colucci.mauro@gmail.com>
2023-11-14 00:03:59 -0300

    getTodos from server in page.tsx

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
-    return [45, 65, 78];
+    return [45, 65, 78, "caba", 7];
   }),
 });
 
